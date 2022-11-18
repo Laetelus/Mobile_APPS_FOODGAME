6fb0f476856c8e854446477bc4f93cc18a874690
Angelo <72906227+Laetelus@users.noreply.github.com>
2022-11-17 23:57:40 -0500

    Draft

---
+++ FoodGame/FoodGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodGame
+//
+//  Created by Phantom on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
