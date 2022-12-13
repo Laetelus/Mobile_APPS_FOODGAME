65695153fead63a6495c7fd30653cd9279479514
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2022-12-12 23:53:23 -0500

    removed old files

--- Unit-8-Group-Project-App-Design-Part-1-main/FoodGame/Pods/Target Support Files/Pods-FoodGame-FoodGameUITests/Pods-FoodGame-FoodGameUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodGame_FoodGameUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FoodGame_FoodGameUITests
-@end
--- Unit-8-Group-Project-App-Design-Part-1-main/FoodGame/Pods/Target Support Files/Pods-FoodGameTests/Pods-FoodGameTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodGameTests : NSObject
-@end
-@implementation PodsDummy_Pods_FoodGameTests
-@end
